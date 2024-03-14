0267457fcb645fb5f1be0cb62d80da9a7a062870
ouchtech <hamza.houchti@livecampus.techcom>
2024-03-14 23:34:36 +0100

    LastPush

---
+++ index.js
@@ -0,0 +1,17 @@
+
+import express  from "express"
+
+const app = express()
+
+
+
+app.get("/", (req, res) => {
+    res.send("Hello!");
+});
+
+
+const PORT = 3000
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT ${PORT}...`)
+})
+
